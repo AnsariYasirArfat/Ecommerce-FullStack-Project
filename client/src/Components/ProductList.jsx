cfd5778005aca22bc22583b8d5e88a56144c88f0
AnsariYasirArfat <ansariyaseer786@gmail.com>
2023-06-13 01:20:53 +0530

    ProductList alignment & responsiveness done

--- client/src/Components/ProductList.jsx
+++ client/src/Components/ProductList.jsx
@@ -147,24 +147,34 @@ function ProductList() {
   ];
 
   return (
-    <>
-      <div>
+    <section>
+      {/* <div>
         <h1 className="text-4xl font-bold text-center p-4 text-teal-900">
           ProductList
         </h1>
-      </div>
+      </div> */}
       <Tabs
         id="custom-animation"
         value="all"
-        className="flex flex-col md:flex-row relative"
+        className="flex flex-col xl:flex-row py-4 "
       >
-        <TabsHeader className="sticky top-0 flex flex-row md:flex-col md:w-32">
-          {data.map(({ label, value }) => (
-            <Tab key={value} value={value} className="bg-teal-100">
-              {label}
-            </Tab>
-          ))}
-        </TabsHeader>
+        <div className=" bg-teal-200 rounded-lg mx-4 mb-8 xl:mb-0 xl:mx-3 shadow-lg shadow-teal-900/60">
+          <h1 className="pt-2 text-center text-base lg:text-xl text-teal-700 font-bold">
+            Category:
+          </h1>
+          <TabsHeader className=" flex flex-row xl:flex-col p-1.5 md:p-2.5 xl:w-40 m-2 lg:m-4 bg-teal-300">
+            {data.map(({ label, value }) => (
+              <Tab
+                key={value}
+                value={value}
+                className="text-xs lg:text-sm font-bold md:py-2 md:my-1"
+              >
+                {label}
+              </Tab>
+            ))}
+          </TabsHeader>
+        </div>
+
         <TabsBody
           animate={{
             initial: { y: 400 },
@@ -173,7 +183,13 @@ function ProductList() {
           }}
         >
           {data.map(({ value, products }) => (
-            <TabPanel key={value} value={value} className="py-0 flex flex-wrap">
+            <TabPanel
+              id="ProductListWindow"
+              key={value}
+              value={value}
+              className="overflow-auto pt-0 pb-4 grid justify-items-center gap-6 grid-cols-2 sm:grid-cols-3 sm:gap-y-10 md:grid-cols-4 xl:grid-cols-4 2xl:grid-cols-4 2xl:gap-y-16 justify-center"
+              style={{ height: "680px" }}
+            >
               {products.map((product) => {
                 return <ProductCard key={product.Name} Product={product} />;
               })}
@@ -181,8 +197,10 @@ function ProductList() {
           ))}
         </TabsBody>
       </Tabs>
-    </>
+    </section>
   );
 }
 
 export default ProductList;
+
+// flex flex-wrap justify-evenly gap-x-1.5 gap-y-6 sm:gap-y-10 md:gap-y-4
