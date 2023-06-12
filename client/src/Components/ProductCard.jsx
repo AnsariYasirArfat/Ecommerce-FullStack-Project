e05503b4d4564865492d2105b7cca9ecaee12cf5
AnsariYasirArfat <ansariyaseer786@gmail.com>
2023-06-13 01:19:43 +0530

    Productcards' size font & Responsivenes updated

--- client/src/Components/ProductCard.jsx
+++ client/src/Components/ProductCard.jsx
@@ -19,15 +19,15 @@ function ProductCard({ Product }) {
   };
   return (
     <>
-      <Card className="w-48 sm:w-52 md:w-60 lg:w-64 bg-opacity-[0.87] hover:bg-opacity-100 shadow-lg shadow-teal-900/60 hover:shadow-xl hover:shadow-teal-900/80">
+      <Card className="justify-between w-40 h-64 md:w-44 md:h-72 lg:w-52 lg:h-80 2xl:w-60 2xl:h-[352px] bg-opacity-[0.87] hover:bg-opacity-100 shadow-lg shadow-teal-900/60 hover:shadow-xl hover:shadow-teal-900/80 ">
         <CardHeader
           shadow={true}
           floated={false}
-          className="h-44 sm:h-48 md:h-56 lg:h-60  m-3 md:m-4 lg:m-5  "
+          className="h-36 md:h-40 lg:h-48  2xl:h-56 m-3 md:m-4 lg:m-5  "
         >
           <img
             src={Product.Image}
-            className="w-full h-full object-center object-cover rounded-lg shadow-2xl shadow-teal-950/50 hover:scale-110 ease-in-out duration-100"
+            className="w-full h-full object-center object-cover rounded-md shadow-2xl shadow-teal-950/50 hover:scale-110 ease-in-out duration-100"
           />
         </CardHeader>
         <CardBody className="px-4 py-0 lg:px-5">
@@ -46,12 +46,12 @@ function ProductCard({ Product }) {
             </Typography>
           </div>
         </CardBody>
-        <CardFooter className="p-3 sm:p-4 md:p-5 ">
+        <CardFooter className="p-3 sm:p-4">
           {isWishlisted ? (
             <Button
               onClick={RemoveFromWishList}
               fullWidth={true}
-              className="text-[10px] md:text-xs lg:text-sm  px-4 py-2 md:py-3 bg-teal-900/10 text-red-500 shadow-none hover:shadow-none  focus:shadow-none focus:scale-105 active:scale-100"
+              className="text-[8.5px] md:text-[9.5px] lg:text-xs px-0 py-2 md:py-3 bg-teal-900/10 text-red-500 shadow-none "
             >
               Remove from WishList
             </Button>
@@ -59,7 +59,7 @@ function ProductCard({ Product }) {
             <Button
               onClick={AddedToWishList}
               fullWidth={true}
-              className="text-[10px] md:text-xs lg:text-sm px-4 py-2 md:py-3 bg-teal-900/10 text-teal-700 shadow-none hover:shadow-none  focus:shadow-none focus:scale-105 active:scale-100"
+              className="text-[9px] md:text-[9.5px] lg:text-xs py-2 md:py-3 bg-teal-900/10 text-teal-700 shadow-none "
             >
               Add to WishList
             </Button>
