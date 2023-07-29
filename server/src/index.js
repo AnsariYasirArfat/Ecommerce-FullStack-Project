6296728d409ea9fb25cac3305562102af4b300ae
AnsariYasirArfat <ansariyaseer786@gmail.com>
2023-07-29 21:40:28 +0530

    Server started and database connected

---
+++ server/src/index.js
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+import app from "./app.js";
+import config from "./config/index.js";
+
+(async () => {
+  try {
+    await mongoose.connect(config.MONGO_URL);
+    console.log("DB Connected!");
+
+    app.on("error", (err) => {
+      console.error("ERROR: ", err);
+      throw err;
+    });
+
+    app.get("/", (req, res) => {
+      res.send("Hello World!");
+    });
+    const onListening = () => {
+      console.log(`listening on port ${config.PORT}`);
+    };
+
+    app.listen(config.PORT, onListening);
+  } catch (err) {
+    console.error("ERROR: ", err);
+    throw err;
+  }
+})();
