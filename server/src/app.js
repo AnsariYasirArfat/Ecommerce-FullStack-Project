f4c3e8fb2ad9a35fa3a8359aaf1df7c95053b85d
AnsariYasirArfat <ansariyaseer786@gmail.com>
2023-07-31 22:54:32 +0530

    express Middleware & CORS, Cookies packages added

--- server/src/app.js
+++ server/src/app.js
@@ -1,5 +1,17 @@
 import express from "express";
-
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import config from "./config/index.js";
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(
+  cors({
+    origin: config.LIVE_URL,
+  })
+);
+
+app.use(cookieParser());
 
 export default app;
--- server/src/config/index.js
+++ server/src/config/index.js
@@ -3,6 +3,7 @@ dotenv.config();
 
 const config = {
   PORT: process.env.PORT || 8000,
+  LIVE_URL: process.env.LIVE_URL,
   MONGO_URL: process.env.MONGO_URL,
   JWT_SECRET: process.env.JWT_SECRET,
   JWT_EXPIRY: process.env.JWT_EXPIRY,
