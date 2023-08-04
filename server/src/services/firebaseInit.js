702fce160db5cce5571e3b1ba2d6c4cb498a035f
AnsariYasirArfat <ansariyaseer786@gmail.com>
2023-08-04 22:59:37 +0530

    firebaseInt file deleted

--- server/src/services/firebaseInit.js
+++
@@ -1,7 +0,0 @@
-import firebase from "firebase/app";
-import "firebase/storage";
-import firebaseConfig from "../config/firebase.config.js";
-
-firebase.initializeApp(firebaseConfig);
-
-export const storage = firebase.storage();
